31f19ebbcc818f4a2a5c6b0230331c97631a8fc7
Alex Parker <3zanders@gmail.com>
2014-08-05 22:47:26 +0100

    moved over app.d completely from meld

--- source/app.d
+++ source/app.d
@@ -1,11 +1,62 @@
 import meld;
+import derelict.opengl3.gl3;
+import std.stdio;
 
 void main()
 {
 	Window window = new Window("Hello, world!", 640, 480);
+	
+	Mesh mesh = Mesh.CreateCube();
+	Camera camera = new Camera(640, 480);
+	Shader shader = new Shader("data/specular.glsl");
+	shader.SetParameter("world", mat4.identity);
+	shader.Bind();
+
+	double currentTime = window.Time();
+	double accumulator = 0.0;
+	double dt = 1.0 / 60.0;
 
 	while (window.IsRunning())
 	{
+		double newTime = window.Time();
+		double frameTime = newTime - currentTime;
+		currentTime = newTime;
+
+		accumulator += frameTime;
+		while (accumulator >= dt)
+		{
+			if (Input.IsKeyDown(Keys.A))
+				camera.Move(dt, 0.0f);
+			if (Input.IsKeyDown(Keys.D))
+				camera.Move(-dt, 0.0f);
+			if (Input.IsKeyDown(Keys.W))
+				camera.Move(0.0f, dt);
+			if (Input.IsKeyDown(Keys.S))
+				camera.Move(0.0f, -dt);
+
+			if (Input.IsKeyDown(Keys.E))
+				camera.Look(dt*2.0, 0.0f);
+			if (Input.IsKeyDown(Keys.Q))
+				camera.Look(-dt*2.0, 0.0f);
+			if (Input.IsKeyDown(Keys.R))
+				camera.Look(0.0f, dt);
+			if (Input.IsKeyDown(Keys.F))
+				camera.Look(0.0f, -dt);
+
+			accumulator -= dt;
+		}
+
+		glClearColor(0.0, 0.2, 0.8, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shader.SetParameter("viewProj", camera.viewProj);
+
+		shader.SetParameter("world", mat4.translate(vec3(2.0f, 0.0f, 20.0f)));
+		mesh.Draw();
+
+		shader.SetParameter("world", mat4.translate(vec3(-2.0f, 0.0f, 20.0f)));
+		mesh.Draw();
+
 		window.Swap();
 	}
 }
